09bdcec804423b1cad73caa51ad77001cbfa8c0c
Max-Jordan <max19992319@yandex.ru>
2023-01-15 19:22:13 +0500

    11

--- src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.item;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.item.dto.ItemDto;
